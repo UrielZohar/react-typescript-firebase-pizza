fce3541807e3b656cd8803cfda226503f01c7807
Uriel Zohar <urielz@boldintel.com>
2025-04-28 18:01:46 +0300

    init sdk

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdkxF2DasENYc-qpJZp3anALBwOC8MxAI",
+  authDomain: "react-typescript-pizza.firebaseapp.com",
+  projectId: "react-typescript-pizza",
+  storageBucket: "react-typescript-pizza.firebasestorage.app",
+  messagingSenderId: "869711572844",
+  appId: "1:869711572844:web:549b9bdc3a79c174918d49",
+  measurementId: "G-LM85CNPD1L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
